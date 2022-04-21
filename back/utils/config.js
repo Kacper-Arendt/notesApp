56eac49107d38caf00fe3e1851e11ed5c81bde1e
ArendtKacper <k.arendt@gridaly.com>
2022-04-21 08:44:02 +0200

    add heroku&firebase deploy

--- back/utils/config.js
+++ back/utils/config.js
@@ -6,6 +6,3 @@ export const PORT =process.env.PORT
 export const DATABASE_URL =process.env.DATABASE_URL
 export const SECRET=process.env.SECRET
 
-export const MONGODB_URI = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_MONGODB_URL
-  : process.env.MONGO_URL
